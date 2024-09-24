9ad1d2432e80a4373446227d810f58ad388e610a
janFrystak <janfrystak@gmail.com>
2024-09-24 11:16:21 +0200

    little cleaning comments (ik we're gonna delete it, but I'm bored )

--- src/BruteForceSearch.java
+++ src/BruteForceSearch.java
@@ -4,28 +4,30 @@ public class BruteForceSearch {
         int mainS = mainString.length();
         int subS = substring.length();
         int currentIndex = 0;
+        if(mainS > subS) {
+            while (true) {
+                //number of found symbols from substring
+                int subIndex = 0;
+                //Curren symbol in sub and main string
+                int savedIndex = currentIndex;
 
-        while (true) {
-            int subIndex = 0;
-            int savedIndex = currentIndex;
-
-            while (subIndex < subS && currentIndex < mainS && mainString.charAt(currentIndex) == substring.charAt(subIndex)) {
+                while (subIndex < subS && currentIndex < mainS && mainString.charAt(currentIndex) == substring.charAt(subIndex)) {
+                    currentIndex++;
+                    subIndex++;
+                }
+                //We found the whole substring
+                if (subIndex == subS) {
+                    return savedIndex;
+                }
+                //At the end of text
+                if (currentIndex == mainS) {
+                    break;
+                }
+                //Move current sub and main string forward
                 currentIndex++;
-                subIndex++;
             }
 
-            if (subIndex == subS) {
-                return savedIndex;
-            }
-
-            if (currentIndex == mainS) {
-                break;
-            }
-
-            currentIndex = savedIndex + 1;
-        }
-
-        return -1;
+        } return -1;
     }
 
     public static void main(String[] args) {
